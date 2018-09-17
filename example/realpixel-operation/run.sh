41c0d48e7bca13b5423f534c8c6dfcc7616a3d0a
Peter Pang <pang0144@e.ntu.edu.sg>
2018-09-17 17:23:02 +0800

    Fix example scripts' running and mod

--- example/realpixel-operation/run.sh
+++ example/realpixel-operation/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # build with spec
+definitions=$(<definitions.yaml)
+configuration=$(<configuration.yaml)
 docker build \
-  --build-arg SWAGGER_SPEC=$(<definitions.yaml) \
-  --build-arg SWAGGER_UI=$(<configuration.yaml) \
-  --build-arg SWAGGER_ENDPOINT=http://localhost:8888/v1 \
   -t realpixel-operation:0.1.0 \
-  ../
+  --build-arg SWAGGER_SPEC="${definitions}" \
+  --build-arg SWAGGER_UI="${configuration}" \
+  --build-arg SWAGGER_ENDPOINT=http://localhost:8888/v1 \
+  ../../ &&
 
 # run locally
 docker run \
-  -p 5000:5000 \
+  -p 3000:5000 \
   realpixel-operation:0.1.0
