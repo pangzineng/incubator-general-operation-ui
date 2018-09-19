b506ffb2a13d66aa17f02fa2a4d82a45782d8d95
Peter Pang <pang0144@e.ntu.edu.sg>
2018-09-19 16:59:28 +0800

    [minor] example realpixel-operation update on data structure

--- example/realpixel-operation/run.sh
+++ example/realpixel-operation/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+CONTAINER=${1:-gou-realpixel-operation}
+VERSION=${2:-0.1.0}
 
 # build with spec
 definitions=$(<definitions.yaml)
 configuration=$(<configuration.yaml)
 docker build \
-  -t realpixel-operation:0.1.0 \
+  -t ${CONTAINER}:${VERSION} \
   --build-arg SWAGGER_SPEC="${definitions}" \
   --build-arg SWAGGER_UI="${configuration}" \
   --build-arg SWAGGER_ENDPOINT=http://localhost:8888/v1 \
@@ -12,5 +14,8 @@ docker build \
 
 # run locally
 docker run \
+  -d \
   -p 3000:5000 \
-  realpixel-operation:0.1.0
+  --name ${CONTAINER} \
+  ${CONTAINER}:${VERSION}
+docker logs -f ${CONTAINER}
