7e2fb22e5de5c24072723d48c011e7e31463af9c
Peter Pang <pang0144@e.ntu.edu.sg>
2018-10-18 16:25:36 +0800

    Fix login/logout bug, clean up header/logo/title

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,10 @@ import green from '@material-ui/core/colors/green';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import {realContract} from '../common/MagicUtil';
 import {loadSwagger} from "../common/SwaggerUtil";
+import { Typography } from "@material-ui/core";
+var _ = require('lodash');
 
 const styles = theme => ({
   appBar: {
@@ -28,17 +29,8 @@ const styles = theme => ({
   },
   logo: {
     flexGrow: 1,
-    height: theme.mixins.toolbar.minHeight*0.9,
     display: 'flex',
-    position: 'relative',
     alignItems: 'center'
-  },
-  logoImg: {
-    width:'5vw',
-    minWidth: '80px',
-    height:'70%',
-    marginLeft: theme.spacing.unit,
-    cursor: 'pointer'
   },
   btn: {
     marginRight: theme.spacing.unit
@@ -103,6 +95,7 @@ class Header extends Component {
     onSetSnacker({openSnack: true, snackMsgType: "success", 
       snackMsg: `You are now logout`
     })
+    this.setState({input: null})
     history.push("/")
   }
 
@@ -126,7 +119,7 @@ class Header extends Component {
           onSetDefinitions(definitions)
           onSetProperties(properties)
           onSetUser({'userID': input, color, profile: {...profile, active: true}})
-          this.setState({loginAction: false}, () => {
+          this.setState({loginAction: false, input: null}, () => {
             onSetSnacker({openSnack: true, snackMsgType: "success", 
               snackMsg: `You are now login`
             })
@@ -139,7 +132,7 @@ class Header extends Component {
       onSetSnacker({openSnack: true, snackMsgType: "error", 
         snackMsg: `Nah, your account is wrong`
       })
-      this.setState({loginAction: false}, () => {
+      this.setState({loginAction: false, input: null}, () => {
         onSetUser(null)
       })  
     })
@@ -159,20 +152,9 @@ class Header extends Component {
             </IconButton>
           </Tooltip>
             <div className={classes.logo} >
-            <Tooltip title="Go Home!">
-            <SvgIcon className={classes.logoImg} onClick={this.handleRootClick} viewBox="0 0 502 210.6">
-              <g>
-                <path fill="#cd201f" d="M498.3 45.7s-2.9-20.4-11.8-29.4c-11.3-11.9-24-12-29.9-12.6-41.7-3-104.2-3-104.2-3h-.2s-62.5 0-104.2 3c-5.8.7-18.5.7-29.9 12.6-8.9 9-11.8 29.4-11.8 29.4s-3 24-3 48v22.5c0 24 3 48 3 48s2.9 20.5 11.8 29.5c11.4 11.8 26.3 11.4 32.9 12.7 23.8 2.2 101.3 3 101.3 3s62.6-.2 104.3-3.2c5.9-.7 18.6-.7 29.9-12.5 9-9 11.8-29.5 11.8-29.5s3-24 3-48V93.7c0-24-3-48-3-48"/>
-                <g>
-                  <path d="M188 169.5h-19v-11c-7.2 8.3-13.3 12.5-20 12.5-5.8 0-9.8-2.8-11.9-7.8-1.2-3-2-7.7-2-14.6V68.7H154v81.9c.4 2.8 1.6 3.8 4 3.8 3.7 0 7-3.2 11-8.8V68.7h19v100.8zM102.1 139.6c1 10-2 15-8 15s-9-5-8-15v-40c-1-9.9 2.1-14.6 8-14.6 6 0 9 4.7 8 14.7v39.9zm19-38c0-10.7-2.2-18.8-6-23.9-5-6.9-13-9.7-21-9.7-9 0-15.9 2.8-21 9.7-3.8 5.1-5.9 13.3-5.9 24v35.9c0 10.7 1.8 18.1 5.7 23.2a26.7 26.7 0 0 0 42.6 0c3.8-5 5.6-12.5 5.6-23.2v-36zM46.2 114.6v55h-20v-55S5.7 47.4 1.4 34.8h21l14 52.6 14-52.6h20.9l-25 79.8z"/>
-                </g>
-                <g fill="#fff">
-                  <path d="M440.4 96.6c0-9.3 2.6-11.8 8.6-11.8s8.4 2.7 8.4 12v10.9h-17v-11zm36 26v-20.4c0-10.6-2.1-18.4-6-23.5a25.3 25.3 0 0 0-21.2-10.4c-9.3 0-16.4 3.6-21.7 10.4-3.8 5-6 13.3-6 24v34.9a37 37 0 0 0 6.3 23 25.9 25.9 0 0 0 21.8 10.2 25 25 0 0 0 26.1-21.6c.2-1.8.6-6 .6-11.8v-2.8h-19v12.5c-1 4.9-3.7 7.3-8.1 7.3-6.2 0-8.9-4.6-8.8-13.8v-18h36zM390.5 140.6c0 10-2.3 13.8-7.5 13.8a14 14 0 0 1-9.5-4.6V89.3c3-3 6.5-4.5 9.5-4.5 5.2 0 7.5 3 7.5 12.9v42.9zm2.1-72.5c-6.6 0-13.2 4.1-19 11.3V35.8h-18v133.7h18v-10c6 7.5 12.6 11.3 19 11.3 7.2 0 12.5-3.8 14.9-11.2 1.2-4.3 2-10.8 2-20v-40a70 70 0 0 0-2.4-19.8c-2.5-7.5-7.3-11.7-14.5-11.7M340.6 169.5h-19v-11c-7.1 8.3-13.2 12.5-19.9 12.5-5.8 0-9.8-2.8-11.9-7.8-1.2-3-2-7.7-2-14.6V69.7h18.9v80.9c.4 2.8 1.6 3.8 4 3.8 3.7 0 7-3.2 11-8.8V69.7h19v99.8z"/>
-                  <path d="M268.8 169.5h-20V54.8h-21v-19h63v19h-22v114.7z"/>
-                </g>
-              </g>
-            </SvgIcon>
-            </Tooltip>
+            <Typography variant="h6">
+              {user.profile ? _.startCase(user.profile.name) : 'General Operation UI'}
+            </Typography>
             </div>
           {
             user['userID'] ?
